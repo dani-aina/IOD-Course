aaca6182c7e3f0b27a3f59c0f9508a7e34c1576b
dani-aina <danica.m.lee@hotmail.com>
2026-06-04 16:57:38 +1000

    js lab exercises

--- Module3/fun.js
+++ Module3/fun.js
@@ -2,7 +2,7 @@
 
 // ============ Exercise 1 ============
 
-"" + 1 + 0; //this will result in: "" + 1
+"" + 1 + 0 //this will result in: "" + 1
 console.log("" + 1 + 0); // 10 is a string not the number 10
 
 /* this actually returned 10 because 
@@ -11,3 +11,22 @@ treats everything as string concatenation
 from that point left-to-right. Since "" comes first, 
 everything after it gets joined as a 
 string rather than added as numbers.*/
+
+"" - 1 + 0 // this will return -1
+
+true + false // this will return 1
+
+!true // this will return false
+
+6 / "3"
+
+/* 
++ with a string is concatenation but
+-, *, / always changes a string into a number
+*/
+
+"2" * "3" // returns 6
+
+4 + 5 + "px" // returns 45px
+
+// it actually returns 9px .... this confused me, ask in class //
