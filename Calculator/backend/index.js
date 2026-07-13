1ae2f3334173f7e89ab76b4ba4734b1d2718d45b
dani-aina <danica.m.lee@hotmail.com>
2026-07-13 22:56:02 +1000

    calculator

---
+++ Calculator/backend/index.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const app = express();
+
+app.get("/calculator/add", (req, res) => {
+  const num1 = parseInt(req.query.num1);
+  const num2 = parseInt(req.query.num2);
+  const result = num1 + num2;
+  res.json({ result: result });
+});
+
+app.get("/calculator/subtract", (req, res) => {
+  const num1 = parseInt(req.query.num1);
+  const num2 = parseInt(req.query.num2);
+  const result = num1 - num2;
+  res.json({ result: result });
+});
+
+app.get("/calculator/multiply", (req, res) => {
+  const num1 = parseInt(req.query.num1);
+  const num2 = parseInt(req.query.num2);
+  const result = num1 * num2;
+  res.json({ result: result });
+});
+
+app.get("/calculator/divide", (req, res) => {
+  const num1 = parseInt(req.query.num1);
+  const num2 = parseInt(req.query.num2);
+  const result = num1 / num2;
+  res.json({ result: result });
+});
+
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
