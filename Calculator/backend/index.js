1114d41faa635798468fa884fa8e7f6b313b7f58
dani-aina <danica.m.lee@hotmail.com>
2026-07-14 19:00:15 +1000

    calculator done

--- Calculator/backend/index.js
+++ Calculator/backend/index.js
@@ -1,5 +1,8 @@
 const express = require("express");
+const path = require("path");
 const app = express();
+
+app.use(express.static(path.join(__dirname, "../frontend")));
 
 app.get("/calculator/add", (req, res) => {
   const num1 = parseInt(req.query.num1);
